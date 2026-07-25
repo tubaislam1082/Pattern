17e06d3fbad1348f5736e12685e55a7b71147bc8
Tubaislam <sanjidaislam1089@gmail.com>
2026-07-25 22:43:43 +0600

    Update print Floyd's pattern Triangle pyramid.c

--- print Floyd's pattern Triangle pyramid.c
+++ print Floyd's pattern Triangle pyramid.c
@@ -2,12 +2,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,k=1;
+    int i,j,n,num=1;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
     for(j=1;j<=i;j++)
-        printf("%d ",k++);
+        printf("%d ",num);
+        num++;
         printf("\n");
     }
 }
