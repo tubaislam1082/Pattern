e7292b10737e570a3396b34991c5ed5fc0a64190
Tubaislam <sanjidaislam1089@gmail.com>
2026-07-25 22:44:50 +0600

    Update print pascal's pattern Triangle pyramid.c

--- print pascal's pattern Triangle pyramid.c
+++ print pascal's pattern Triangle pyramid.c
@@ -2,15 +2,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,n,c;
+    int i,j,n,num;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        c=1;
+        num=1;
         for(j=0;j<=i;j++)
         {
-        printf("%d ",c);
-        c=c*(i-j)/(j+1);
+        printf("%d ",num);
+        num=num*(i-j)/(j+1);
         }
         printf("\n");
     }
